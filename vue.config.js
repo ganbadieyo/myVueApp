a519aa3eb197d1b200b1a8df5fb7130d57b1a41d
ganbadieyo <1124520115@qq.com>
2022-03-01 21:09:48 +0800

    修复

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,7 @@ function resolve (dir) {
 }
 module.exports = {
     lintOnSave: true,
+    publicPath: './',
     chainWebpack: (config)=>{
         config.resolve.alias
             .set('@', resolve('src'))
